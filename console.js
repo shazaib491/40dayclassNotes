52c702e0448aea7cd60e3627fae040a497081855
JavaEcommerce <shazaibrehman127@gmail.com>
2024-09-24 00:19:50 +0530

    24-sept-2024

---
+++ console.js
@@ -0,0 +1,73 @@
+// console.log("Starting");
+
+// console.info("something is running start");
+// console.info("something is running end ");
+
+// let arr = [1, 2, 3, 4, 5];
+// console.table(arr);
+
+// console.clear();
+
+// console.error("something is wrog");
+
+// console.time("start");
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+// console.timeEnd("start");
+
+// // calcualting 2 no sum of
+// console.group();
+// let a = 10;
+// let b = 20;
+// console.log(a + b);
+// console.log(a + b);
+// console.log(a + b);
+// console.log(a + b);
+// console.log(a + b);
+// console.groupEnd();
+
+// /*
+//     this function is used for calculating the sum of 2 no 
+//     console.,njlen  
+//     dlfnsalkfn
+// */
+
+
+// /**
+//  * multi line no with js annotation 
+//  * calcuating the sum of 10 natural no 
+//  */
+// /* 
+// let s=0;
+// for(let i=1; i<=10; i++){
+//     s+=i;
+// }  
+    
+// */
+
+// console.log(5**3)
+
+
+// let s=10;
+// // s=s+20;
+// s+=20;
+// console.log(s)
+
+// s=s-5;
+// s-=5;
+
+// console.log(s);
+
+
+// let sum="jack";
+
+// console.log(sum);
+
+// let a=200;
+// let b=100;
+// let c=40;
+// let exr=a > b &&  a > c ? 'a is greater': b>a && b > c ? 'b is greater' : 'c is greater';
+
+// console.log(typeof 1.20)
+
---
+++ nested.js
@@ -0,0 +1,110 @@
+var num = 9;
+
+// if (num > 10) {
+//   if (num % 2 == 0) {
+//     console.log("no is even");
+//   } else {
+//     console.log("no is odd");
+//   }
+// } else {
+//   console.log("no is not greater than 10");
+// }
+
+// var s = 100;
+// var y = 200;
+
+// if (s % 2 == 0 && y % 1 !== 0) {
+// }
+// num==10
+// switch (num) {
+//   case 1:
+//     console.log("no is one");
+//     break;
+//   case 10:
+//     console.log("no is 10");
+//     break;
+//   case 11:
+//     console.log("no is 11")
+//     break;
+//   case 13:
+//     console.log("no is 13");
+//     break;
+//   default:
+//     console.log("no is not greater than 10");
+// }
+
+// switch(num > 10){
+//     case num % 10:
+//         console.log("no is greater than 10");
+//         break;
+//     case num==10:
+//         console.log("no is equtal to 10")
+//         break;
+//     default:
+//         console.log("no is not greater than 10");
+// }
+
+// let days=3;
+
+// switch(days){
+//     case 1:
+//         console.log("Monday")
+//         break;
+//     case 2:
+//         console.log("Tuesday")
+//         break;
+//     case 3:
+//     console.log("Thrusday")
+//     break;
+//     default:
+//         console.log("Sunday")
+//         break;
+// }
+
+// let user="admin";
+
+// switch(user){
+//     case "admin":
+//         console.log("user is admin")
+//     case "shazaib":
+//         console.log("user is shazaib");
+//         break;
+//     default:
+//         console.log("invalid user")
+// }
+
+// let no = 20;
+
+// switch (no) {
+//   case 10:
+//     console.log("number is 10");
+//   case 20:
+//     console.log("number is 20");
+//   case 30:
+//     console.log("number is 30");
+//   default:
+//     console.log("number is invalid ");
+// }
+
+//
+
+// while loop
+// do while
+// for loop
+
+// wap to print number from 1 to 5
+let count = 1;
+while (count <= 5) {
+  console.log(count);
+  count++;
+}
+count=1;
+console.log("----------------------------------")
+do {
+  console.log(count);
+  count++;
+} while (count <= 5);
+console.log("----------------------------------")
+for(;count <=7;){
+    console.log(count);
+}
