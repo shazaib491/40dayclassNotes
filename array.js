03e9559842adff22a20f05ef92265a52f815f590
JavaEcommerce <shazaibrehman127@gmail.com>
2024-09-26 00:10:14 +0530

    array notes

---
+++ array.js
@@ -0,0 +1,13 @@
+const cars=[1,2,3,4,5];
+// console.table(cars.length)
+// //           0        1             2     3              
+
+// // wap to print all element in array using for loop
+// for(var i=0; i<=cars.length-1; i++){
+//     console.log(cars[i])
+// }
+
+
+for(var element of cars){
+    console.log(element)
+}
--- function.js
+++ function.js
@@ -3,97 +3,50 @@
 // //     s+=i;
 // // }
 
-
 // // console.log(s);
 
 //             // arguemnt
-// function sum(startValue, endValue){ //decleration of function 
+// function sum(startValue, endValue){ //decleration of function
 //     let s=0;
 //     for(let i=startValue; i<=endValue; i++){
 //         s+=i;
 //     }
 //     return s;
-//     //defination of function 
+//     //defination of function
 // }
-
-
-
-
 
 // console.log(sum(10,20)) //execute
 // console.log(sum(1,10))
 
-
 // console.log("Hello world");
-
 
 // function message(msg){
 //     console.log(msg);
 // }
 
-
-
 // message("India")
 
-
-// regular function 
-
-// pass nothing return nothing 
-function printHello(){
-    console.log("Hello World")
-}
-
-printHello()
-
-// pass something return nothing 
-function passSomething(params){
-    console.log(params);
-}
-
-passSomething("Adeeb khan")
-
-// pass nothing return something 
-function passNothingReturnSomething(){
-    let s=1+20;
-    return s;
-}
-
-let sumofnumber=passNothingReturnSomething();
-
-console.log(sumofnumber % 2 == 0 ? 'number is valid':'invalid number');
-
-// pass something return something 
-function sumOfTwoNumbers(a,b){
-    let s=a+b;
-    return s;
-}
-
-let ans=sumOfTwoNumbers(100,50)
-console.log(ans*30);
-
+// regular function
 
 // {
 //     console.log(a)
 //     var a=10;
 // }
 
+console.log(answer());
 
-console.log(answer())
-
-function answer(){
-    var a=200;
-    console.log(a);
+function answer() {
+  var a = 200;
+  console.log(a);
 }
 
-console.log(product(10,20));
+console.log(product(10, 20));
 
-function product(a,b){
-    return a*b;
+function product(a, b) {
+  return a * b;
 }
 
 // console.log(a);
-
-
 
 // function expressions
 // var print=function(){
@@ -102,6 +55,150 @@ function product(a,b){
 
 // print();
 
+// console.log(a);
+// let  a=10;
+
+// for(let a=1; a<=20; a++){
+//     console.log(a);
+// }
+
+// console.log("Hello world");
+
+// var a=20;
+// let a=200;
+
+// console.log(a);
+
+// for(let i=0;;i++){}
+
+// console.log(i);
+
+// pass something return something
+
+// function expression
+// function hello() {
+//   console.log("hello");
+// }
+
+// pass nothing return nothing
+function printHello() {
+  console.log("Hello World");
+}
+
+printHello();
+
+// pass something return nothing
+function passSomething(params) {
+  console.log(params);
+}
+
+passSomething("Adeeb khan");
+
+// pass nothing return something
+function passNothingReturnSomething() {
+  let s = 1 + 20;
+  return s;
+}
+
+let sumofnumber = passNothingReturnSomething();
+
+console.log(sumofnumber % 2 == 0 ? "number is valid" : "invalid number");
+
+// pass something return something
+function sumOfTwoNumbers(a, b) {
+  let s = a + b;
+  return s;
+}
+
+let ans = sumOfTwoNumbers(100, 50);
+console.log(ans * 30);
+
+myWorld();
+function myWorld() {
+  console.log("disneyLand");
+}
+
+//anonymous function
+// 1.)function expression
+var hello = function () {
+  console.log("Hello world");
+};
+
+// var sum=function(a,b){
+//     console.log(a+b)
+// }
+
+// sum(10,20)
+
+// var mulitply=function(){
+//     let product=10*20;
+//     return product
+// }
+
+// console.log(mulitply())
+
+// var divide=function(a,b){
+//     return a/b;
+// }
+
+// console.log(divide(10, 20));
+
+// 2) arrow function
+// console.log(printMessage)
+// const  printMessage = () => {
+//   console.log("Message");
+// };
 
 
+// printMessage();
 
+var sum = (a, b) => {
+  console.log(a + b);
+};
+
+sum(10, 20);
+
+var mulitply = () => {
+  let product = 10 * 20;
+  return product;
+};
+
+console.log(mulitply());
+
+var divide = (a, b) => {
+  return a / b;
+};
+
+
+var printHelloWorld=()=>console.log("Hello")
+var calculate=(a,b)=>a+b;
+console.log(calculate(12,2))
+printHelloWorld();
+
+
+// iife function 
+// immediatley invoke function exectuion 
+console.log((function(a,b){
+    console.log("jsdnjklsdnfjsdjklfsdjkfljsdnjlfnsdl",10,20)
+    return 10+20;
+})(10,20));
+
+
+(()=>console.log("Samjhe"))();
+
+var a=10;
+{
+    let a=100;
+    console.log(a)
+// console.log(a)
+}
+
+{
+    console.log(a);
+    let nae="admin";
+console.log(nae)
+
+}
+
+
+console.log(a)
