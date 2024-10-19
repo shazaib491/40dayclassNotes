65572e70d4407990bd872559562e858da7e60282
shazaib491 <shazaibrehman127@gmail.com>
2024-10-19 23:56:44 +0530

    commi

---
+++ wind.js
@@ -0,0 +1,107 @@
+/**
+ * 
+ * window 
+ * this 
+ * localstoge
+ * classes
+ * imort export 
+ * defer
+ * splice
+ * 
+ */
+
+
+// console.log(window.a)
+
+// var a=10;
+
+
+// // let ans=window.confirm("do you want to deleted record")
+// // console.log(document);
+// console.log(window.innerHeight)
+// console.log(window.innerWidth)
+
+// console.log(window.document.cookie)
+
+// console.log(this);
+
+// this.alert("deleted");
+
+// function regular(){
+//     console.log(this);
+// }
+
+// regular();
+
+
+// function print(){
+
+// }
+
+
+// let print=function(){
+
+// }
+
+
+// function regular(){
+//     console.log(this);
+// }
+
+// regular();
+// let obj={
+//     name:'admin',
+//     email:'admin@example.com',
+//     showname:function(){
+//         setTimeout(()=>{
+//             console.log(this);
+//         },1000)
+//     }
+// }
+
+// obj.showname();
+
+
+// function Person(name,email,password){
+//     this.name=name;
+//     this.email=email;
+//     this.password=password;
+//     setTimeout(()=>{
+//         console.log(this);
+//     },1000);
+// }
+
+
+
+// Person.prototype.showEmail=function(){
+//     console.log(this.email);
+// }
+
+// var person=new Person('admin',"admin@gmail.com","admin#223");
+// // person.showName();
+// person.showEmail();
+
+// console.log(window.person);
+
+// localStorage.setItem("name","Daud DD");
+
+
+// let name=localStorage.getItem("name");
+
+// let person={
+//     name:'admin'
+// }
+
+// localStorage.removeItem("name");
+
+// localStorage.setItem('user',JSON.stringify(person));
+// let personObj=localStorage.getItem('user');
+// let parsePerson=JSON.parse(personObj)
+// console.log(parsePerson);
+
+// localStorage.clear();
+
+sessionStorage.setItem("name","admin");
+
+
+document.cookie="username=inaya; expires=Thu, 01 Jan 1970 00:00:00 UTC;  path=/"
